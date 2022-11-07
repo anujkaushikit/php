b41ea4c44b2bae97b626be4e182abbff9f62aef0
Anuj Kaushik <55687624+anujkaushikit@users.noreply.github.com>
2022-11-07 18:13:07 +0530

    Parameters_Arguments_with_default_value
    Parameters_Arguments_with_default_value

---
+++ Parameters_Arguments_with_default_value.php
@@ -0,0 +1,26 @@
+<?php
+
+function sum(){ // simple function
+    $num_1 = 10;
+    $num_2 = 5;
+    $result = $num_1 + $num_2;
+    echo "number {$num_1} and number {$num_2} sum result is.{$result} </br>";
+}
+sum();
+    
+function sum_with_parameters($num_1,$num_2){ // when you create a function that time you pass parameters in function 
+
+    $result = $num_1 + $num_2;
+    echo "two number sum with parameters, number one is {$num_1} and number two is {$num_2} sum result is.{$result} </br>";
+}
+sum_with_parameters(25,20);// when you call a function that time you pass the value behalf of parameter is called argument in function 
+    
+
+function multiply_with_parameters($num_1, $num_2 = 5){ // when you create a function that time you pass parameters in function 
+    $result = $num_1 * $num_2;
+    echo "two number multiply with parameters and with default value is 5, number one is {$num_1} and number two is {$num_2} multiply result is.{$result} </br>";
+}
+multiply_with_parameters(25,20);// when you call a function that time you pass the value behalf of parameter is called argument in function 
+
+        
+?>
